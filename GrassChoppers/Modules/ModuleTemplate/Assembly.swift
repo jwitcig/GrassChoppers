e37f508902e1a4ce45cf7b775bf706a20abd8594
Jonah Witcig <jwitcig@gmail.com>
2020-07-30 23:30:44 -0500

    added prompt module assembly

--- GrassChoppers/Modules/ModuleTemplate/Assembly.swift
+++ GrassChoppers/Modules/ModuleTemplate/Assembly.swift
@@ -2,18 +2,50 @@ import Swinject
 
 final class AssemblyClass: Assembly {
     
+    typealias Router = Routing
+    
     private let container: Container
+    private let routerFactory: (Resolver) -> Routing
     
     init(
-        parentContainer: Container
+        parentContainer: Container,
+        routerFactory: @escaping (Resolver) -> Routing
     ) {
         self.container = Container(parent: parentContainer)
+        self.routerFactory = routerFactory
         
         privatelyAssemble()
     }
 
     private func privatelyAssemble() {
         
+        container.register(ViewController.self) { resolver in
+            return ViewController(
+                logicController: resolver.resolve(LogicControlling.self)!
+            )
+        }.initCompleted { resolver, viewController in
+            resolver.resolve(Routing.self)!.viewController = viewController
+        }
+        
+        container.register(LogicControlling.self) { resolver in
+            return LogicController(
+                dataManager: resolver.resolve(DataManaging.self)!,
+                router: resolver.resolve(Routing.self)!
+            )
+        }
+        
+        container.register(DataManaging.self) { resolver in
+            return DataManager()
+        }
+        
+        container.register(Router.self, factory: routerFactory)
+        
+        container.register(LogicControlling.self) { resolver in
+            return LogicController(
+                dataManager: resolver.resolve(DataManaging.self)!,
+                router: resolver.resolve(Routing.self)!
+            )
+        }
     }
     
     func assemble(container: Container) {
--- GrassChoppers/Prompt/PromptAssembly.swift
+++ GrassChoppers/Prompt/PromptAssembly.swift
@@ -2,18 +2,50 @@ import Swinject
 
 final class PromptAssembly: Assembly {
     
+    typealias Router = PromptRouting
+    
     private let container: Container
+    private let routerFactory: (Resolver) -> Router
     
     init(
-        parentContainer: Container
+        parentContainer: Container,
+        routerFactory: @escaping (Resolver) -> Router
     ) {
         self.container = Container(parent: parentContainer)
+        self.routerFactory = routerFactory
         
         privatelyAssemble()
     }
 
     private func privatelyAssemble() {
         
+        container.register(PromptViewController.self) { resolver in
+            return PromptViewController(
+                logicController: resolver.resolve(PromptLogicControlling.self)!
+            )
+        }.initCompleted { resolver, viewController in
+            resolver.resolve(PromptRouting.self)!.viewController = viewController
+        }
+        
+        container.register(PromptLogicControlling.self) { resolver in
+            return PromptLogicController(
+                dataManager: resolver.resolve(PromptDataManaging.self)!,
+                router: resolver.resolve(PromptRouting.self)!
+            )
+        }
+        
+        container.register(PromptDataManaging.self) { resolver in
+            return PromptDataManager()
+        }
+        
+        container.register(Router.self, factory: routerFactory)
+        
+        container.register(PromptLogicControlling.self) { resolver in
+            return PromptLogicController(
+                dataManager: resolver.resolve(PromptDataManaging.self)!,
+                router: resolver.resolve(PromptRouting.self)!
+            )
+        }
     }
     
     func assemble(container: Container) {
